be97985a371cbfc9345d541619590e5bc89ccf37
tropotek <info@tropotek.com>
2016-11-14 10:15:50 +1100

    Minor Code Updates - 252s-dev.vet.unimelb.edu.au

--- src/config/application.php
+++ src/config/application.php
@@ -1,29 +1,27 @@
 <?php
 /*
+ * Application default config values
+ * This file should not need to be edited
+ *
  * @author Michael Mifsud <info@tropotek.com>
  * @link http://www.tropotek.com/
  * @license Copyright 2015 Michael Mifsud
  */
 $config = \Tk\Config::getInstance();
 
-/**
- * Config the session using PHP option names prepended with 'session.'
- * @see http://php.net/session.configuration
- */
 include_once(__DIR__ . '/session.php');
 
+/*
+ * If you use sub folders in your URL's you
+ * must define the site root paths manually.
+ */
+//$config['site.path'] = dirname(dirname(dirname(__FILE__)));
+//$config['site.url'] = dirname($_SERVER['PHP_SELF']);
 
 /*
  * Change the system timezone
  */
 //$config['date.timezone'] = 'Australia/Victoria';
 
-
-/*
- * If you use sub folders in your URL's you 
- * must define the site root paths manually.
- */
-//$config['site.path'] = dirname(dirname(dirname(__FILE__)));
-//$config['site.url'] = dirname($_SERVER['PHP_SELF']);
 
 
